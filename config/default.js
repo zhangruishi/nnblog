3e6fb1be0dde689e196a9032497e72a93828c86e
zhangruishi <zhangruishi0817@gmail.com>
2017-06-21 13:55:08 +0800

    first

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 3000,
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://Amiao:123456@ds147551.mlab.com:47551/zhangruishi_nblog'
+};
